2a0e203402a7686b65768275acad0d23c28796b5
ahaouem <96834553+ahaouem@users.noreply.github.com>
2023-09-30 19:57:53 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -46,7 +46,7 @@ const HomePage = () => {
 
   return (
     <div className="p-4 bg-gray-100 min-h-screen">
-      <h1 className="text-2xl font-bold mb-6 text-center">Home Page</h1>
+      <h1 className="text-2xl font-bold mb-6 text-center text-black">Home Page</h1>
       <div className="flex justify-center">
         <form className="w-1/3">
           <div className="mb-4">
