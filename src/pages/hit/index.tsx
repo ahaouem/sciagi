47850d1dc227f871d4ef25d2f93d4c9f44344d8f
ahaouem <96834553+ahaouem@users.noreply.github.com>
2025-03-26 11:41:20 +0100

    feat: sciaga

--- src/pages/hit/index.tsx
+++ src/pages/hit/index.tsx
@@ -49,12 +49,12 @@ Relacje z władzą sądowniczą:
 - Powołuje wszystkich sędziów
 - Powołuje pierwszego prezesa sądu najwyższego oraz trybunału konstytucyjnego
 
-	Wojciech Jaruzelski – 31.12.1989 – 22.12.1990
-	2.	Lech Wałęsa – 22.12.1990 – 23.12.1995
-	3.	Aleksander Kwaśniewski – 23.12.1995 – 23.12.2005 (dwie kadencje)
-	4.	Lech Kaczyński – 23.12.2005 – 10.04.2010 (zginął w katastrofie smoleńskiej)
-	5.	Bronisław Komorowski – 06.08.2010 – 06.08.2015
-	6.	Andrzej Duda – 06.08.2015 – obecnie (druga kadencja zakończy się w 2025)
+	Wojciech Jaruzelski – 31.12.1989 – 22.12.1990 - wprowadzenie stanu wojennego
+	2.	Lech Wałęsa – 22.12.1990 – 23.12.1995 - wycofanie wojsk radzieckich  z polski
+	3.	Aleksander Kwaśniewski – 23.12.1995 – 23.12.2005 (dwie kadencje) - on wygral bez drugiej tury
+	4.	Lech Kaczyński – 23.12.2005 – 10.04.2010 (zginął w katastrofie smoleńskiej) - prezydent i rzad z obozu postsolidarnosciowego ktory prezydent byl wtedy
+	5.	Bronisław Komorowski – 06.08.2010 – 06.08.2015 - a wladza ustawodawcza i wykonawcza z przewaga po
+	6.	Andrzej Duda – 06.08.2015 – obecnie (druga kadencja zakończy się w 2025) - dwie kadencje
 `;
 
 const Home: React.FC = () => {
